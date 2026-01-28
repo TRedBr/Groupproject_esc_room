407ce9f5ec9082444578651ce4851dbd8b13b5b9
Schlumf1 <tim.knischewski@gmx.de>
2026-01-28 15:06:03 +0100

    inital_rooms_logic_branch

--- rooms.py
+++ rooms.py
@@ -1,2 +1,23 @@
 #Manages the rooms, and their intern logic for returning false and true from the answers
 #creates functions per room, and provides the room index numbers
+
+class Room:
+    def __init__(self, name, task, answer):
+        self.name = name
+        self.task = task
+        self.__answer = answer
+
+    def get_antwort(self):
+        return self.__antwort
+
+    def room_execute(self):
+        print(f"\nHello! Welcome at the {self.name}.")
+        print(f"Task: {self.task}")
+        enter =  input("Please enter your answer: ")
+
+        if enter == self.__answer:
+            return True
+        else:
+            return False
+
+r1 = Room("Math-Door", "What is (5+10)*2?", "30")
