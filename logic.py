1b86d9db4356d4f4d263d3303655460248091c8c
Schlumf1 <tim.knischewski@gmx.de>
2026-01-29 13:18:57 +0100

    rooms_logic

--- logic.py
+++ logic.py
@@ -1,3 +1,51 @@
-#this will contain the logic of game tracking the players progress(score) responsible for User interaction if answers are true or false, and processes
-#if player advances a room(score) or repeats it. Checks if all rooms are cleared.
+# this will contain the logic of game tracking the players progress(score) responsible for User interaction if answers are true or false, and processes
+# if player advances a room(score) or repeats it. Checks if all rooms are cleared.
 ##requires data.py and rooms.py
+from rooms import r1, r2, r3
+from data import get_userscores, check_user_exist, get_userprogress, save_userdata
+
+
+class Game:
+    def __init__(self):
+        self.rooms = [r1, r2, r3]
+        self.current_room_index = 0
+        self.username = ""
+        self.userscores = get_userscores()
+
+    def new_game(self):
+        self.username = input("Please enter your username: ").strip()
+
+        if not check_user_exist(self.userscores, self.username):
+            self.userscores[self.username] = 0
+        print(f"Welcome {self.username}, let's start the game!")
+
+        while self.current_room_index < len(self.rooms):
+            current_room = self.rooms[self.current_room_index]
+
+            while not current_room.room_execute():
+                pass
+
+            self.current_room_index += 1
+            save_userdata(self.userscores)
+
+        print("Congratulations! You've completed the Game.")
+
+        self.userscores[self.username] = self.current_room_index
+        save_userdata(self.userscores)
+
+    def load_game(self):
+        if check_user_exist(self.userscores, self.username):
+            user_score = get_userprogress(self.userscores, self.username)
+            self.current_room_index = user_score
+            print(f"Welcome back {self.username}, resuming from room {self.current_room_index} + 1.")
+
+        while self.current_room_index < len(self.rooms):
+            current_room = self.rooms[self.current_room_index]
+
+            while not current_room.room_execute():
+                pass
+
+            self.current_room_index += 1
+            save_userdata(self.userscores)
+
+        print("Congratulations! You've completed the Game.")
--- rooms.py
+++ rooms.py
@@ -1,5 +1,5 @@
-#Manages the rooms, and their intern logic for returning false and true from the answers
-#creates functions per room, and provides the room index numbers
+# Manages the rooms, and their intern logic for returning false and true from the answers
+# creates functions per room, and provides the room index numbers
 
 class Room:
     def __init__(self, name, task, answer):
@@ -7,17 +7,34 @@ class Room:
         self.task = task
         self.__answer = answer
 
-    def get_antwort(self):
-        return self.__antwort
+    def get_answer(self):
+        return self.__answer
 
     def room_execute(self):
         print(f"\nHello! Welcome at the {self.name}.")
         print(f"Task: {self.task}")
-        enter =  input("Please enter your answer: ")
+        user_input = input("Please enter your answer: ").strip()
 
-        if enter == self.__answer:
+        if user_input == self.__answer:
+            print("Correct answer.")
             return True
         else:
+            print("Incorrect answer, try again.")
             return False
 
-r1 = Room("Math-Door", "What is (5+10)*2?", "30")
+
+r1 = Room(
+    "Math-Door",
+    "What is (5+10)*2?",
+    "30"
+)
+r2 = Room(
+    "Number-Filter-Panel",
+    "Enter all the even numbers from this list: [3,10,15,22,7]",
+    "10 22"
+)
+r3 = Room(
+    "Password-Terminal",
+    "Enter the password. (Hint: python + 123",
+    "python123"
+)
